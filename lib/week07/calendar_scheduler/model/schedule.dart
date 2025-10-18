e722722e5ee73a14421e2fce3827b8d8877bd92c
thengelsec <ssuk83158315@gmail.com>
2025-10-18 15:43:53 +0900

    feat: Add date and startTime columns to Schedules table

--- lib/week07/calendar_scheduler/model/schedule.dart
+++ lib/week07/calendar_scheduler/model/schedule.dart
@@ -2,6 +2,7 @@ import 'package:drift/drift.dart';
 
 class Schedules extends Table {
   IntColumn get id => integer().autoIncrement()();  // PRIMARY KEY, 정수 열
-  TextColumn get content => text()();
-  
+  TextColumn get content => text()();       // 내용, 글자 열
+  DateTimeColumn get date => dateTime()();  // 일정 날짜, 날짜 열
+  Intcolumn get startTime
 }
