e4892411b733eba51c84a83d164c187c8fbed7a1
thengelsec <ssuk83158315@gmail.com>
2025-10-18 15:51:53 +0900

    feat: Import necessary dependencies for Drift database setup

--- lib/week07/calendar_scheduler/database/drift_database.dart
+++ lib/week07/calendar_scheduler/database/drift_database.dart
@@ -1,6 +1,11 @@
 import 'package:myapp/week07/calendar_scheduler/model/schedule.dart';
 import 'package:drift/drift.dart';
 
+import 'package:drift/native.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:path/path.dart';
+import 'dart:io';
+
 // private값까지 불러올 수 있음
 part 'drift_database.g.dart'; // part 파일 지정
 
