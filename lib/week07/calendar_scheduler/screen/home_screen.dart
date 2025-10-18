88f564515fb16cfda7836dadfbbfa41b60d0700c
thengelsec <ssuk83158315@gmail.com>
2025-10-18 15:41:53 +0900

    Refactor: Move calendar scheduler code to week07

--- lib/week07/calendar_scheduler/screen/home_screen.dart
+++ lib/week07/calendar_scheduler/screen/home_screen.dart
@@ -1,9 +1,9 @@
-import 'package:myapp/week06/calendar_scheduler/component/main_calendar.dart';
+import 'package:myapp/week07/calendar_scheduler/component/main_calendar.dart';
 import 'package:flutter/material.dart';
-import 'package:myapp/week06/calendar_scheduler/component/schedule_card.dart';
-import 'package:myapp/week06/calendar_scheduler/component/today_banner.dart';
-import 'package:myapp/week06/calendar_scheduler/component/schedule_bottom_sheet.dart';
-import 'package:myapp/week06/calendar_scheduler/const/colors.dart';
+import 'package:myapp/week07/calendar_scheduler/component/schedule_card.dart';
+import 'package:myapp/week07/calendar_scheduler/component/today_banner.dart';
+import 'package:myapp/week07/calendar_scheduler/component/schedule_bottom_sheet.dart';
+import 'package:myapp/week07/calendar_scheduler/const/colors.dart';
 
 
 class HomeScreen extends StatefulWidget {
