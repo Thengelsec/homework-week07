8325ca4aadc781c629732fefe2cd0914a8bb0bae
thengelsec <ssuk83158315@gmail.com>
2025-10-18 15:46:23 +0900

    feat: Initialize Drift database with Schedule table

---
+++ lib/week07/calendar_scheduler/database/drift_database.dart
@@ -0,0 +1,11 @@
+import 'package:myapp/week07/calendar_scheduler/model/schedule.dart';
+import 'package:drift/drift.dart';
+
+// private값까지 불러올 수 있음
+part 'drift_database.g.dart'; // part 파일 지정
+
+@DriftDatabase(
+  tables: [
+    Schedules,
+  ],
+)
