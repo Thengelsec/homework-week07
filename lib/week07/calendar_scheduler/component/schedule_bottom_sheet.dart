e0a5fd2e0faf1ed77c61faf77775911fa994a3ab
thengelsec <ssuk83158315@gmail.com>
2025-10-18 16:00:23 +0900

    feat: Wrap schedule bottom sheet with Form widget

--- lib/week07/calendar_scheduler/component/schedule_bottom_sheet.dart
+++ lib/week07/calendar_scheduler/component/schedule_bottom_sheet.dart
@@ -15,13 +15,14 @@ class _ScheduleBottomSheetState extends State<ScheduleBottomSheet> {
   int? startTime;   // 시작 시간 저장 변수
   int? endTime;     // 종료 시간 저장 변수
   String? content;  // 일정 내용 저장 변수
-  
+
   @override
   Widget build(BuildContext context) {
     // 키보드 높이 가져오기
     final bottomInset = MediaQuery.of(context).viewInsets.bottom;
 
-    return SafeArea(
+    return Form(    // 텍스트 필드를 한번에 관리할 수 있는 폼
+      key: formKey, // Form을 조작할 키값
       child: Container(
         // 화면에 반 높이에 키보드 높이 추가하기
         height: MediaQuery.of(context).size.height / 2 + bottomInset, //
