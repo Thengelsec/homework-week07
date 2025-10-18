bc21f92af17c671193c445011a0a357e9db0419e
thengelsec <ssuk83158315@gmail.com>
2025-10-18 15:53:53 +0900

    feat: Implement Drift database connection and path configuration

--- lib/week07/calendar_scheduler/database/drift_database.dart
+++ lib/week07/calendar_scheduler/database/drift_database.dart
@@ -16,7 +16,7 @@ part 'drift_database.g.dart'; // part 파일 지정
 )
 class LocalDatabase extends _$LocalDatabase {
   LocalDatabase() : super(_openConnection());
-  
+
   Stream<List<Schedule>> watchSchedules(DateTime date) =>
   // 데이터를 조회하고 변화 감지
     (select(schedules)..where((tbl) => tbl.date.equals(date))).watch();
@@ -31,3 +31,14 @@ class LocalDatabase extends _$LocalDatabase {
   int get schemaVersion => 1;
 }
 // Code Generation으로 생성할 클래스 상송
+
+// LocalDatabase 클래스 아래에 작성
+LazyDatabase _openConnection() {
+  return LazyDatabase(() async {
+
+    // 데이터베이스 파일 저장할 폴더
+    final dbFolder = await getApplicationDocumentsDirectory();
+    final file = File(p.join(dbFolder.path, 'db.sqlite'));
+    return NativeDatabase(file);
+  });
+}
