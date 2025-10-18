a4a530a8d2ec2d5944a2dfc14734f25ed138bb47
thengelsec <ssuk83158315@gmail.com>
2025-10-18 16:14:54 +0900

    feat(calendar_scheduler): Inject database instance using GetIt

--- lib/week07/calendar_scheduler/screen/home_screen.dart
+++ lib/week07/calendar_scheduler/screen/home_screen.dart
@@ -4,7 +4,8 @@ import 'package:myapp/week07/calendar_scheduler/component/schedule_card.dart';
 import 'package:myapp/week07/calendar_scheduler/component/today_banner.dart';
 import 'package:myapp/week07/calendar_scheduler/component/schedule_bottom_sheet.dart';
 import 'package:myapp/week07/calendar_scheduler/const/colors.dart';
-
+import 'package:get_it/get_it.dart';
+import 'package:myapp/week07/calendar_scheduler/database/drift_database.dart';
 
 class HomeScreen extends StatefulWidget {
   // StatelessWidget에서 StatefulWidget으로 전환
